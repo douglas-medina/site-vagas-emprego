acbf7d3cb9b181ff448651e6e510489338c11efb
Douglas Medina <medina.douglas.dmv@gmail.com>
2024-08-17 02:19:08 -0300

    reestruturação

--- vagas/admin.py
+++ vagas/admin.py
@@ -1,8 +1,3 @@
 from django.contrib import admin
-from .models import Vaga, Candidatura
-
 
 # Register your models here.
-
-admin.site.register(Vaga)
-admin.site.register(Candidatura)
--- vagas/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from . import views
-
-url_patterns = [
-    path("vagas/<int:vaga_id>/", views.vaga_detalhes, name="vaga_detalhes"),
-]
